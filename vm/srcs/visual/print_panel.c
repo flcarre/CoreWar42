d9a00d33727ae20f341d83558fe79f488d5802ce
Daniel COLOMA <11773186+dacoloma@users.noreply.github.com>
2020-03-10 15:39:43 +0100

    NORME

--- vm/srcs/visual/print_panel.c
+++ vm/srcs/visual/print_panel.c
@@ -31,10 +31,8 @@ void	ft_print_war_bis(t_win *info, int x, int y)
 	wattroff(info->window, COLOR_PAIR(COLOR_P1));
 	y += 3;
 	x -= 12;
-	info->coord.x = x;
-	while (x < info->dim.cols - 1)
-		mvwprintw(info->window, y, x++, "-");
-	info->coord.y = y + 3;
+	mvwprintw(info->window, y, x,
+		"----------------------------------------------------------");
 }
 
 void	ft_print_first_panel(t_win *info, t_vm *vm)
@@ -44,7 +42,7 @@ void	ft_print_first_panel(t_win *info, t_vm *vm)
 	int	i;
 
 	x = info->coord.x + 8;
-	y = info->coord.y;
+	y = info->coord.y + 18;
 	i = ft_get_player_color(vm, vm->last_live->id);
 	mvwprintw(info->window, y++, x, "current cycle\t: %6d", vm->cycles);
 	mvwprintw(info->window, y++, x, "Cycles/second\t: %6d", vm->visu->cps);
@@ -56,9 +54,8 @@ void	ft_print_first_panel(t_win *info, t_vm *vm)
 	wattroff(info->window, COLOR_PAIR(vm->visu->color_p[i + 1]) | A_BOLD);
 	y += 3;
 	x = info->coord.x;
-	while (x < info->dim.cols - 1)
-		mvwprintw(info->window, y, x++, "-");
-	info->coord.y = y + 3;
+	mvwprintw(info->window, y, x,
+		"----------------------------------------------------------");
 }
 
 void	ft_print_secnd_panel(t_win *info, t_vm *vm)
@@ -67,7 +64,7 @@ void	ft_print_secnd_panel(t_win *info, t_vm *vm)
 	int	y;
 
 	x = info->coord.x + 8;
-	y = info->coord.y;
+	y = info->coord.y  + 27;
 	mvwprintw(info->window, y++, x, "%-25s %d", "Number of players:",
 		vm->nb_players);
 	mvwprintw(info->window, y++, x, "%-25s %d", "Number of processes:",
@@ -79,9 +76,8 @@ void	ft_print_secnd_panel(t_win *info, t_vm *vm)
 	wattroff(info->window, A_STANDOUT);
 	y += 3;
 	x = info->coord.x;
-	while (x < info->dim.cols - 1)
-		mvwprintw(info->window, y, x++, "-");
-	info->coord.y = y + 3;
+	mvwprintw(info->window, y, x,
+		"----------------------------------------------------------");
 }
 
 void	ft_print_third_panel(t_win *info, t_vm *vm)
@@ -93,7 +89,7 @@ void	ft_print_third_panel(t_win *info, t_vm *vm)
 	t_visu	*visu;
 
 	x = info->coord.x + 8;
-	y = info->coord.y;
+	y = info->coord.y + 35;
 	player = 0;
 	visu = vm->visu;
 	while (player < vm->nb_players)
@@ -102,7 +98,7 @@ void	ft_print_third_panel(t_win *info, t_vm *vm)
 		{
 			i = ft_get_player_color(vm, vm->live_tab[player]);
 			wattron(info->window, COLOR_PAIR(visu->color_p[i + 1]));
-			mvwprintw(info->window, y++, x, "(%d)%-20.20s has called Live!",
+			mvwprintw(info->window, y++, x, "(%d)%-.20s has called Live!",
 				vm->player[i].id, vm->player[i].name);
 			wattroff(info->window, COLOR_PAIR(visu->color_p[i + 1]));
 		}
@@ -115,13 +111,14 @@ void	ft_print_fourth_panel(t_win *info, char *state)
 	int		x;
 	int		y;
 
-	x = 1;
-	y = 45;
-	while (x < info->dim.cols - 1)
-		mvwprintw(info->window, y, x++, "-");
-	y = 55;
-	x = 20;
+	x = info->coord.x;
+	y = info->coord.y + 58;
+	mvwprintw(info->window, y, x,
+		"----------------------------------------------------------");
+	y = 63;
+	x = 23;
 	wattron(info->window, A_BOLD);
 	mvwprintw(info->window, y, x, "**%s**\t", state);
+	mvwprintw(info->window, y + 1, x, "COLS = %d\tLINES = %d", COLS, LINES);
 	wattroff(info->window, A_BOLD);
 }
