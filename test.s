c6ce6ea2c9e5f876b007827b2e9791dadb745007
Daniel COLOMA <11773186+dacoloma@users.noreply.github.com>
2020-03-11 18:07:12 +0100

    UPDATE test code for champ

--- test.s
+++ test.s
@@ -1,14 +1,6 @@
 .name "Test long long long long long name"
 .comment "This is a test.."
 
-#add r2,r3,r5
-#zjmp %:live
-sti r1,%:live, %1
-live: live %1
-ld %-64, r3
-sti r3,%49,%2
-ld %9,r2
-sti r2,%38,%-3
-ld %0 , r2
-#add r2,r3,r5
-#zjmp %100
+trap:
+	ld 		%0, r2
+	zjmp	%:trap
